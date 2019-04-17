b6c8c4c24b7343276d1ba983586ee61844cd4935
Michael Osakoh <macmive@gmail.com>
2019-04-17 20:02:58 +0100

    product class added in shop model

--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 
 
+# a category contains many products
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
@@ -16,3 +17,30 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('shop: product_list_by_category', args=[self.slug])
+
+
+# a product belongs to a category
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    # DecimalField is used for money
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('created',)
+        # to query  products using id & slug. Combined together to improve performance
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop: product_detail', args=[self.id, self.slug])
+
