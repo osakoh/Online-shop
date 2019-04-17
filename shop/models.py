4230debbadd14a87d38779a45f582b7dab605d6c
Michael Osakoh <macmive@gmail.com>
2019-04-17 19:41:01 +0100

    added category model in shop app

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop: product_list_by_category', args=[self.slug])
